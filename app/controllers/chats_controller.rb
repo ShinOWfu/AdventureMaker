370b5f5e79b7918082baf57c6b218ed836f5c01e
Jonathan Rogenes <jonathanrogenes@gmail.com>
2025-11-20 15:22:32 +0900

    changed final button

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,5 +2,6 @@ class ChatsController < ApplicationController
   def show
     @chat = current_user.chats.find(params[:id])
     @message = Message.new
+    @user_message_count = @chat.messages.where(role: 'user').count
   end
 end
