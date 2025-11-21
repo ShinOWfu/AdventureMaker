3a03a05b2768e27f0fffff49e81fb73a6c738dd3
Oliver Will <o.will@alumni.maastrichtuniversity.nl>
2025-11-21 16:09:13 +0900

    Hopefully running

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -22,7 +22,11 @@ respond with one short paragraph (3-6 sentences) that immersively describes the 
         end
 
         # image generation
-        ImageGeneratorJob.perform_later(@chat, @last_assistant_message)
+        image_chat = RubyLLM.chat(model: "gemini-2.5-flash-image")
+        reply = image_chat.ask("Generate an image based on this text #{@last_assistant_message.content} and use the attached picture of the protagonist", with: { image: @chat.story.protagonist_image.url })
+        image = reply.content[:attachments][0].source
+        @last_assistant_message.image.attach(io: image, filename: "#.png", content_type: "image/png")
+        @last_assistant_message.save
         redirect_to chat_path(@chat)
 
       rescue RubyLLM::Error => e
