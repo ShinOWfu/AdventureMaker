d5ffef650588a90303b805087034877967dda239
Oliver Will <o.will@alumni.maastrichtuniversity.nl>
2025-11-21 15:54:52 +0900

    for heroku

--- app/jobs/image_generator_job.rb
+++ app/jobs/image_generator_job.rb
@@ -8,6 +8,6 @@ class ImageGeneratorJob < ApplicationJob
     image = reply.content[:attachments][0].source
     last_assistant_message.image.attach(io: image, filename: "#.png", content_type: "image/png")
     last_assistant_message.save
-    Turbo::StreamsChannel.broadcast_update_to(chat, target: dom_id(last_assistant_message), partial: "messages/image", locals: { message: last_assistant_message })
+    Turbo::StreamsChannel.broadcast_replace_to(chat, target: dom_id(last_assistant_message), partial: "messages/image", locals: { message: last_assistant_message })
   end
 end
